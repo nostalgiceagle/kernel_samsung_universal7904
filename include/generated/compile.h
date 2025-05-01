070275cd2bb6a7ffeb621aedf2e069aeb226b98a
donut <76439683+donut2008@users.noreply.github.com>
2025-05-01 22:33:40 +0530

    fs: include: Generations
    Had to commit fs/include/config and fs/include/generated for the same reason.

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 98 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#98 SMP PREEMPT Thu May 1 16:28:09 IST 2025"
+#define LINUX_COMPILE_BY "donut"
+#define LINUX_COMPILE_HOST "DESKTOP-G2HQ3Q2"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
