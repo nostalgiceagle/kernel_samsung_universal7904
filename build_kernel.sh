f79c3eed30ec083673ab73654baff8ea1cff356d
donut6150 <76439683+donut6150@users.noreply.github.com>
2025-07-29 16:50:54 +0530

    Update build script
    Signed-off-by: donut6150 <76439683+donut6150@users.noreply.github.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,14 +4,17 @@ set -euo pipefail
 [[ -n "${3:-}" ]] && set "-$3"
 trap "echo KeyboardInterrupt!; exit 1" INT
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+RED='31m'
+GREEN='32m'
+WHITE='97m'
+NC='[0m'
+BOLD='[1;'
 DEVICE=$1
 DTB=$2
 DTB_YN=$([[ "$DTB" == "dtb" ]] && echo " with DTB" || echo " without DTB")
 ROOT_DIR=$(pwd)
 
+git submodule update --init --recursive
 cd KernelSU-Next/
 COMMIT_TAG=$(git describe --tags)
 cd ..
@@ -19,27 +22,38 @@ cd ..
 LOCALVER="-KSUN-${COMMIT_TAG}"
 
 clean() {
+    echo -e "\n\e${BOLD}${WHITE}========="
+    echo -e "| CLEAN |"
+    echo -e "=========\e${NC}\n"
+
     cd ${ROOT_DIR}/
-    echo "Current directory is $PWD"
-    echo "Make clean..."
     make clean -j4 2>&1 | tee log_clean.log
     rm -rf include/generated/ include/config/
 }
 
 defconfig() {
-    echo "Make defconfig..."
+    echo -e "\n\e${BOLD}${WHITE}============="
+    echo -e "| DEFCONFIG |"
+    echo -e "=============\e${NC}\n"
+
     make LOCALVERSION=\"${LOCALVER}\" ARCH=arm64 -j4 "exynos7885-${DEVICE}_oneui_defconfig" 2>&1 | tee "log_${DEVICE}_defconfig.log"
 }
 
 kernel() {
-    echo "Make kernel..."
+    echo -e "\n\e${BOLD}${WHITE}=========="
+    echo -e "| KERNEL |"
+    echo -e "==========\e${NC}\n"
+
     make LOCALVERSION=\"${LOCALVER}\" ARCH=arm64 -j4 2>&1 | tee log_${DEVICE}_kernel.log
 }
 
 zip_kernel() {
-    echo -e "${GREEN}Build succeeded!"
+    echo -e "\n\e${BOLD}${GREEN}===================="
+    echo -e "| Build succeeded! |"
+    echo -e "====================\e${NC}\n"
+
     KERNEL_VERSION=$(grep UTS_RELEASE include/generated/utsrelease.h | cut -d'"' -f2 | sed 's/+*$//')
-    echo -e "Kernel version: ${KERNEL_VERSION}"
+    echo -e "\e[${GREEN}Kernel version: ${KERNEL_VERSION}"
     
     cp arch/arm64/boot/Image AnyKernel3/Image
     FILES=(
@@ -55,9 +69,13 @@ zip_kernel() {
     )
     
     if [ "${DTB}" == "dtb" ]; then
-        echo -e "Building DTB..."
+        echo -e "\n\e${BOLD}${WHITE}======="
+        echo -e "| DTB |"
+        echo -e "=======\e${NC}\n"
+
         make ARCH=arm64 -j4 dtb.img 2>&1 | tee log_dtb.log
         cp arch/arm64/boot/dtb.img AnyKernel3/dtb.img
+
         FILES+=("dtb.img")
     fi
     
@@ -65,35 +83,39 @@ zip_kernel() {
 
     ZIPNAME="$(tr '[:lower:]' '[:upper:]' <<< ${DEVICE:0:1})${DEVICE:1} ${KERNEL_VERSION}.zip"
 
-    echo "Current directory is $PWD"
-
+    echo -e "\n\e${BOLD}${WHITE}=============="
+    echo -e "| ZIP KERNEL |"
+    echo -e "==============\e${NC}\n"
     cd AnyKernel3/ 
     zip -r9 "${ZIPNAME}" "${FILES[@]}"
     mv "${ZIPNAME}" "${ROOT_DIR}/"
     cd ${ROOT_DIR}/
 
-    echo -e "Kernel zip: $PWD/${ZIPNAME}${NC}"
-    echo -e "Done${NC}"
+    echo -e "\e${BOLD}${GREEN}Kernel zip: $PWD/${ZIPNAME}"
+    echo -e "Done\e${NC}"
 
     sed -i "1s/.*/KERNELVERSION/" ${ROOT_DIR}/AnyKernel3/version
 }
 
 main() {
-    echo "KernelSU-Next version: ${COMMIT_TAG}"
-    echo "Local version: ${LOCALVER}"
+    echo -e "\nKernelSU-Next ${COMMIT_TAG}"
     start_time=$(date +%s)
+
     clean
     defconfig
     kernel
+
     end_time=$(date +%s)
     if [ -s "arch/arm64/boot/Image" ]; then
         zip_kernel
     else
-        echo -e "${RED}Build failed, reason should be above this message${NC}"
+        echo -e "\n\e${BOLD}${RED}=============================================="
+        echo -e "| Build failed, reason should be above this! |"
+        echo -e "==============================================\e${NC}\n"
     fi
     clean
     elapsed_time=$((end_time - start_time))
-    echo -e "Took $(printf '%02d minutes, %02d seconds' $((elapsed_time%3600/60)) $((elapsed_time%60)))"
+    echo -e "Took $(printf '%02d minutes, %02d seconds' $((elapsed_time%3600/60)) $((elapsed_time%60)))\n"
 }
 
 main
