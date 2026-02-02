5d63a2feadcc0836644073204daa4e431e82eb16
nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>
2026-02-02 07:57:51 +0000

    net: core: pass boolean arg to __sk_attach_prog call
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -1320,7 +1320,7 @@ int sk_attach_bpf(u32 ufd, struct sock *sk)
 	if (IS_ERR(prog))
 		return PTR_ERR(prog);
 
-	err = __sk_attach_prog(prog, sk);
+	err = __sk_attach_prog(prog, sk, false);
 	if (err < 0) {
 		bpf_prog_put(prog);
 		return err;
